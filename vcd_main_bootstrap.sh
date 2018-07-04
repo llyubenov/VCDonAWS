a4e14279421a7a1add693ee808ff9da7e5e54ce9
Lyubomir Lyubenov <llyubomir@gmail.com>
2018-07-04 14:51:47 -0600

    fixed vCD configure script, vCDAdmin varialbe and Console Proxy cloudwatch config

--- vcd_main_bootstrap.sh
+++ vcd_main_bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 # xFer Server bootstrapping
 
+# Redirect script output to a log file
+exec > /var/log/vcd_main_bootstrap.sh                                                                      
+exec 2>&1
 
 # Configuration
 PROGRAM='vCD xFer Server'
@@ -46,6 +49,7 @@ function osrelease () {
 function vcd_main_on_cent_os () {
     # Prepare Variables
     export Region=`curl http://169.254.169.254/latest/meta-data/placement/availability-zone | rev | cut -c 2- | rev`
+    export AZ=`curl http://169.254.169.254/latest/meta-data/placement/availability-zone`
     export vCDBuildBucketName=`curl http://169.254.169.254/latest/user-data/ | grep vCDBuildBucketName | sed 's/vCDBuildBucketName=//g'`
     export vCDBuildName=`curl http://169.254.169.254/latest/user-data/ | grep vCDBuildName | sed 's/vCDBuildName=//g'`
     export vCDKeystoreFileName=`curl http://169.254.169.254/latest/user-data/ | grep vCDKeystoreFileName | sed 's/vCDKeystoreFileName=//g'`
@@ -60,13 +64,13 @@ function vcd_main_on_cent_os () {
     export vCDAdminPasswd=`curl http://169.254.169.254/latest/user-data/ | grep vCDAdminPasswd | sed 's/vCDAdminPasswd=//g'`
     export vCDSystemName=`curl http://169.254.169.254/latest/user-data/ | grep vCDSystemName | sed 's/vCDSystemName=//g'`
     export vCDSerialNumber=`curl http://169.254.169.254/latest/user-data/ | grep vCDSerialNumber | sed 's/vCDSerialNumber=//g'`
-    export vCDAdmin`curl http://169.254.169.254/latest/user-data/ | grep vCDAdmin | sed 's/vCDAdmin=//g'`
+    export vCDAdmin=`curl http://169.254.169.254/latest/user-data/ | grep vCDAdmin | sed 's/vCDAdmin=//g'`
     export MessagesLogGroup=`curl http://169.254.169.254/latest/user-data/ | grep MessagesLogGroup | sed 's/MessagesLogGroup=//g'`
     export CellLogGroup=`curl http://169.254.169.254/latest/user-data/ | grep CellLogGroup | sed 's/CellLogGroup=//g'`
     export ConsoleProxyLogGroup=`curl http://169.254.169.254/latest/user-data/ | grep ConsoleProxyLogGroup | sed 's/ConsoleProxyLogGroup=//g'`
     export vCloudContainerDebugLogGroup=`curl http://169.254.169.254/latest/user-data/ | grep vCloudContainerDebugLogGroup | sed 's/vCloudContainerDebugLogGroup=//g'`
     export efsID=`curl http://169.254.169.254/latest/user-data/ | grep efsID | sed 's/efsID=//g'`
-    export xFerIP=`aws ec2 describe-instances --region=$Region --filters "Name=availability-zone,Values=us-west-2a" "Name=tag:Name,Values='vCD Transfer Server'" --query 'Reservations[*].Instances[*].[PrivateIpAddress]' | sed -n '4p' | sed -e 's/ //g' -e 's/^"//' -e 's/"$//'`
+    export xFerIP=`aws ec2 describe-instances --region=$Region --filters "Name=availability-zone,Values=$AZ" "Name=tag:Name,Values='vCD Transfer Server'" --query 'Reservations[*].Instances[*].[PrivateIpAddress]' | sed -n '4p' | sed -e 's/ //g' -e 's/^"//' -e 's/"$//'`
     export instanceIP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4/)
 
 
@@ -85,7 +89,6 @@ function vcd_main_on_cent_os () {
     sudo yum -y install build/amazon-efs-utils*rpm
     
     #Prepare Transfer Store Mount
-    mkdir -p /opt/vmware/vcloud-director/data/transfer
     echo "$xFerIP:/exports/xfer /opt/vmware/vcloud-director/data/transfer nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev 0 0" >> /etc/fstab
     echo "$efsID /opt/vmware/vcloud-director/data/transfer/cells efs defaults,_netdev 0 0" >> /etc/fstab
 
@@ -93,7 +96,7 @@ function vcd_main_on_cent_os () {
     echo Y | /tmp/$vCDBuildName
 
     #Configure vCD Cell
-    echo Y | /opt/vmware/vcloud-director/bin/configure -cons $instanceIP --console-proxy-port-https 8443 -ip $instanceIP --primary-port-http 80 --primary-port-https 443 -dbhost $DBEndpoint -dbport 5432 -dbtype postgres -dbname $DBName -dbuser $DBMasterUsername -dbpassword "$DBMasterUserPassword" -k /tmp/$vCDKeystoreFileName -w "$VcdCertKeystorePasswd" -unattended
+    /opt/vmware/vcloud-director/bin/configure -ip $instanceIP --primary-port-http 80 --primary-port-https 443 -cons $instanceIP --console-proxy-port-https 8443 -dbhost $DBEndpoint -dbport 5432 -dbtype postgres -dbname $DBName -dbuser $DBMasterUsername -dbpassword $DBMasterUserPassword --keystore /tmp/vCDKeystoreFileNam -w $VcdCertKeystorePasswd  --enable-ceip true -unattended
 
     #Additional changes to vCD global.properties file
 cat >> /opt/vmware/vcloud-director/etc/global.properties <<- EOF
@@ -105,6 +108,9 @@ vcloud.http.maxThreads = 150
 vcloud.http.minThreads = 32
 vcloud.http.acceptorThreads = 16
 hibernate.generate_statistics=true
+
+# Limit the number of active syncs per Subscribed Public Catalog
+contentLibrary.item.sync.batch.size = 1
 
 #Configure vCD to cleanup abandoned DB Threads
 database.pool.removeAbandoned = true
@@ -166,20 +172,21 @@ initial_position = start_of_file
 EOF
     cat /tmp/vCloudContainerDebugLogGroup.txt >> ~/cloudwatchlog.conf
 
-at <<'EOF' >> ~/cloudwatchlog.conf
+cat <<'EOF' >> ~/cloudwatchlog.conf
 
-[console-proxy.log.log]
+[console-proxy.log]
 datetime_format = %b %d %H:%M:%S
-file = /opt/vmware/vcloud-director/logs/console-proxy.log.log
+file = /opt/vmware/vcloud-director/logs/console-proxy.log
 buffer_duration = 5000
 log_stream_name = {hostname}
 initial_position = start_of_file
 EOF
-    cat /tmp/console-proxy.log.txt >> ~/cloudwatchlog.conf
+    cat /tmp/ConsoleProxyLogGroup.txt >> ~/cloudwatchlog.conf
 
     curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
     chmod +x ./awslogs-agent-setup.py
     ./awslogs-agent-setup.py -n -r $Region -c ~/cloudwatchlog.conf
+
 cat <<'EOF' >> /etc/systemd/system/awslogs.service
 [Unit]
 Description=The CloudWatch Logs agent
@@ -198,6 +205,11 @@ WantedBy=multi-user.target
 EOF
     systemctl enable awslogs
     systemctl restart awslogs
+
+    #Start and Stop vCD Cell Services
+    service vmware-vcd start
+    sleep 120
+    service vmware-vcd stop
 
     #Run security updates
 cat <<'EOF' >> ~/mycron
